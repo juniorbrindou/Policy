c24b4afe5018bca82f93f703935b71e514c5dd2a
225junior <--get>
2020-12-05 02:52:03 +0000

    add routes

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/users','UserController@all')->middleware('auth')->name('users');
+Route::get('/users','UserController@index')->middleware('auth')->name('users');
+Route::Post('users/restaurer/{user}','UserController@restaure')->middleware('auth')->name('users.restaurer');
+Route::delete('users/destroy/{user}','UserController@destroy')->middleware('auth')->name('users.destroy');
+Route::get('users/trash','UserController@showDeleted')->middleware('auth')->name('users.trash');
