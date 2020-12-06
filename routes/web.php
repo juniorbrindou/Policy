f33fb1f8997945d53dbcb5072cd4bd08148a096a
225junior <--get>
2020-12-06 23:10:32 +0000

    add group of route

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,19 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Inspiring;
 
 
-Route::get('/', function () {
+Route::GET('/', function () {
 	$quote = Inspiring::quote();
     return view('welcome',compact('quote'));
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::GET('/home', 'HomeController@index')->name('home');
 
-Route::get('/users','UserController@index')->middleware('auth')->name('users');
-Route::Post('users/restaurer/{user}','UserController@restaure')->middleware('auth')->name('users.restaurer');
-Route::delete('users/destroy/{user}','UserController@destroy')->middleware('auth')->name('users.destroy');
-Route::get('users/trash','UserController@showDeleted')->middleware('auth')->name('users.trash');
+
+Route::name('users.')->middleware('auth')->group(function () {
+	Route::PUT('/users/restaurer/{user}','UserController@restaure')->name('restaurer');
+	Route::GET('/users','UserController@index')->name('users');
+	Route::DELETE('destroy/{user}','UserController@destroy')->name('destroy');
+	Route::GET('trash','UserController@showDeleted')->name('trash');
+});
