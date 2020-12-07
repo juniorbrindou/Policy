8279338ab072337d2b83228bedf9806d568c05a5
225junior <--get>
2020-12-07 00:09:55 +0000

    bloqué

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,6 +33,7 @@ class UserController extends Controller
 // suppression
 	public function destroy(User $user)
 	{
+		$this->authorize('delete',$user);
 		User::destroy($user->id);
 		return back()->with('info','Suppression Reussie!');
 	}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-
+use App\User;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
+        User::class => UserPolicy::class,
     ];
 
     /**
