ab4fd76d700278a7bbce3925ed43a2fcbafc69f5
225junior <--get>
2020-12-11 09:03:20 +0000

    debut  affichage des droits

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,25 +9,27 @@
 
                 <div class="card-body">
 
-<div class="alert @can('index',User::class) alert-info @endcan @cannot('index',User::class) alert-danger @endcan ">
+<div class="alert @can('index',App\User::class) alert-info @endcan @cannot('index',App\User::class) alert-danger @endcan ">
                         <p>Lister les utilisateurs</p>
                     </div>
-                    <div class="alert @can('destroy',App\User::class) alert-info @endcan ">
+<div class="alert @can('destroy',App\User::class) alert-info @endcan @cannot('index',App\User::class) alert-danger @endcan ">
                         <p>Supprimer les utilisateurs</p>
                     </div>
-                    <div class="alert @can('update',App\User::class) alert-info @endcan ">
+<div class="alert @can('update',App\User::class) alert-info   @endcan @cannot('index',App\User::class) alert-danger @endcan ">
                         <p>modiffier les utilisateurs</p>
                     </div>
-                    <div class="alert @can('create',App\User::class) alert-info @endcan ">
+<div class="alert @can('create',App\User::class) alert-info   @endcan @cannot('index',App\User::class) alert-danger @endcan ">
                         <p>creer les utilisateurs</p>
                     </div>
-                    <div class="alert @can('restaure',App\User::class) alert-info @endcan ">
+<div class="alert @can('restaure',App\User::class) alert-info  @endcan @cannot('index',App\User::class) alert-danger  @endcan ">
                         <p>Lister les utilisateurs</p>
                     </div>
-                    <div class="alert @can('forceDelete',App\User::class) alert-info @endcan ">
+<div class="alert @can('forceDelete',App\User::class) alert-info   @endcan @cannot('index',App\User::class) alert-danger @endcan ">
                         <p>Supprimer definitivement les utilisateurs</p>
                     </div>
-
+<div class="alert @can('showDeleted',App\User::class) alert-info @endcan">
+    <p>Lister la corbeille</p>
+</div>
                 </div>
             </div>
         </div>
